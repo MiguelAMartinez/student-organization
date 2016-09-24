bb98716802a5e2bf0e24ddf5aee08fcccce91831
Miguel <miguel.martinez@utexas.edu>
2016-09-23 21:11:30 -0500

    fix: remove console.log

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $('.nav-menu-item').click(function() {
 
 $(window).load(function(){
 	$('.nav-ul-submenu').each(function() {
-	  console.log($(this).position().top);
 	  $(this).css('top', ($(this).position().top - 23) + 'px');
 	})
 });
