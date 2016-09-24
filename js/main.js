2d105f157d5810a6e91c303acab72930e66b2585
Miguel <miguel.martinez@utexas.edu>
2016-09-23 20:57:47 -0500

    fix: execute top function only once

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 
 
-
-
-
 $('.nav-menu-item').click(function() {
   window.location = $(this).find("a").attr("href"); 
   return false;
 });
 
-$('.nav-ul-submenu').each(function() {
-  $(this).css('top', ($(this).position().top - 23) + 'px');
-})
+var executed = false;
+
+if (!executed) {
+	$('.nav-ul-submenu').each(function() {
+	  $(this).css('top', ($(this).position().top - 23) + 'px');
+	})	
+	executed = true;
+}
+
+
 
 
 
