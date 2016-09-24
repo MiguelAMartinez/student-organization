1bd096263786cdd065fe92d28457637d6d3776de
Miguel <miguel.martinez@utexas.edu>
2016-09-23 20:33:14 -0500

    fix: update jQuery code for top of submenu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 
 
+
+
 $('.nav-menu-item').click(function() {
   window.location = $(this).find("a").attr("href"); 
   return false;
 });
 
 $('.nav-ul-submenu').each(function() {
-  $(this).css('top', (parseFloat($(this).css('top')) - 20) + 'px');
+  $(this).css('top', ($(this).position().top - 23) + 'px');
 })
-
-
 
 
 
