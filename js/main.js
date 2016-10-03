6ec8dd699fb0c2534ba53e2d14880de29545d2ce
Miguel <miguel.martinez@utexas.edu>
2016-10-03 04:21:28 -0500

    feat: add interactivity in side nav bar

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 
+// JS
 
-$('.nav-menu-item').click(function() {
-	window.location = $(this).find("a").attr("href"); 
-	return false;
-});
-
-$(window).load(function(){
-	$('.nav-ul-submenu').each(function() {
-		$(this).css('top', ($(this).position().top - 31) + 'px');
-	})
-});
 
 	
 
