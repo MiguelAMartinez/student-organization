ea182caece528a094e7846036048043f7f0c9c7a
Miguel <miguel.martinez@utexas.edu>
2016-10-16 22:46:11 -0500

    fix: correct expandable menu and submenu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 
+$('.ul-nav-submenu').before('<div class="div-a-sign">+</div>');
+
 if ($(window).width() <= 900) {
-	var counter = true;	
-	$('.ul-nav-submenu').siblings('.a-li-menu').append('<div class="div-a-sign">+</div>');
+	var smallWindow = true;	
+	$('.ul-nav-submenu').siblings('.a-li-menu').addClass('a-li-menu-sub');
 } else {
-	var counter = false;	
+	var smallWindow = false;	
+	$('.div-a-sign').hide();
 }
 
 $(window).on('resize', function(event){
-	if ($(window).width() <= 900 && counter === false) {
-		counter = true;
+	if ($(window).width() <= 900 && smallWindow === false) {
+		$('.ul-nav-submenu').siblings('.a-li-menu').addClass('a-li-menu-sub');
+		$('.div-a-sign').show();
 		$('.div-ul-menu').hide();
-		$('.ul-nav-submenu').siblings('.a-li-menu').append('<div class="div-a-sign">+</div>');		
-	} else if ($(window).width() > 900 && counter === true) {
+		smallWindow = true;
+	} else if ($(window).width() > 900 && smallWindow === true) {
+		$('.a-li-menu-sub').removeClass('a-li-menu-sub');	
+		$('.div-a-sign').hide();	
 		$('.div-ul-menu').show();
-		$('.div-a-sign').remove();
-		counter = false;
+		$('.ul-nav-submenu').hide();
+		smallWindow = false;
 	}
 });
 
@@ -23,22 +29,20 @@ $('.div-btn').click(function() {
 		$('.div-ul-menu').slideDown(200);
 	} else {
 		$('.div-ul-menu').slideUp(200);
-		$('.a-li-menu').siblings('.ul-2').slideUp(200);
-		$('.a-li-menu').find('.div-a-sign').text('+');
+		$('.a-li-menu').siblings('.ul-nav-submenu').slideUp(200);
+		$('.div-a-sign').text('+');
 	}
-})
+});
 
-$('.ul-nav-submenu').siblings('.a-li-menu').click(function(event){
-	if ($(this).siblings('.ul-nav-submenu').is(':hidden') && $(window).width() <= 900) {
-		event.preventDefault(); 
+$('.div-a-sign').click(function(event){
+	if ($(this).siblings('.ul-nav-submenu').is(':hidden')) {
 		$(this).siblings('.ul-nav-submenu').slideDown(200);
-		$(this).find('.div-a-sign').text('-');
-	} else if ($(window).width() <= 900) {
-		event.preventDefault();
+		$(this).text('-');
+	} else {
 		$(this).siblings('.ul-nav-submenu').slideUp(200);
-		$(this).find('.div-a-sign').text('+');
+		$(this).text('+');
 	}
-})
+});
 
 
 	
